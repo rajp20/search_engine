afa73cb45e383377166dc789f01913e80ba3bdf9
Raj <rajpatel0820@gmail.com>
2020-04-15 15:28:37 -0600

    java server running

--- backend/src/Server.java
+++ backend/src/Server.java
@@ -7,8 +7,8 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.net.URI;
-
-import org.json.simple.JSONObject;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Server {
 
@@ -16,28 +16,44 @@ public class Server {
         HttpServer server = HttpServer.create(new InetSocketAddress("localhost", 8000), 0);
         HttpContext context = server.createContext("/search");
         context.setHandler(Server::handleSearch);
-        System.out.println("Started Server...");
+        System.out.println("Started Server...\n");
         server.start();
     }
 
-    public  static void handleSearch(HttpExchange exchange) throws IOException {
-        System.out.println("-- query --");
+    public static void handleSearch(HttpExchange exchange) throws IOException {
         URI requestURI = exchange.getRequestURI();
-        String query = requestURI.getQuery();
-        System.out.println(query);
+        Map<String, String> query = queryToMap(requestURI.getQuery());
+        String searchQuery = query.get("search_query");
+        System.out.println("Search query: " + searchQuery);
+        System.out.println();
 
         Headers headers = exchange.getResponseHeaders();
         headers.add("Content-Type", "application/json");
         headers.add("Access-Control-Allow-Origin", "*");
 
-        JSONObject obj = new JSONObject();
-        obj.put("Result", "Hello World!");
+        String response = SearchEngine.search(searchQuery);
 
-        String response = "Hello World!";
+        System.out.println("JSON Response:");
+        System.out.println(response);
+        System.out.println();
+
         exchange.sendResponseHeaders(200, response.getBytes().length);
         OutputStream os = exchange.getResponseBody();
         os.write(response.getBytes());
         os.close();
+    }
+
+    public static Map<String, String> queryToMap(String query) {
+        Map<String, String> result = new HashMap<>();
+        for (String param : query.split("&")) {
+            String[] entry = param.split("=");
+            if (entry.length > 1) {
+                result.put(entry[0], entry[1]);
+            }else{
+                result.put(entry[0], "");
+            }
+        }
+        return result;
     }
 
 }
