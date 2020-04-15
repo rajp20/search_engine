3dabd33463236412c040bd88c03c5d22298feab1
Raj <rajpatel0820@gmail.com>
2020-04-15 17:32:39 -0600

    added galago files

--- backend/src/SearchEngine.java
+++ backend/src/SearchEngine.java
@@ -1,5 +1,18 @@
-//comment
 import org.json.simple.JSONObject;
+
+import org.lemurproject.galago.core.index.Index;
+import org.lemurproject.galago.core.index.corpus.DocumentReader;
+import org.lemurproject.galago.core.index.stats.FieldStatistics;
+import org.lemurproject.galago.core.index.stats.IndexPartStatistics;
+import org.lemurproject.galago.core.parse.Document;
+import org.lemurproject.galago.core.parse.Document.DocumentComponents;
+import org.lemurproject.galago.core.retrieval.*;
+import org.lemurproject.galago.core.retrieval.iterator.LengthsIterator;
+import org.lemurproject.galago.core.retrieval.processing.ScoringContext;
+import org.lemurproject.galago.core.retrieval.query.Node;
+import org.lemurproject.galago.core.retrieval.query.StructuredQuery;
+import org.lemurproject.galago.utility.Parameters;
+import org.lemurproject.galago.core.*;
 
 public class SearchEngine {
 
